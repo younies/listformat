9ef369589458132872b73d4dcf0c00e16a7d6db1
Younies Mahmoud <younies@younies-imacpro.roam.corp.google.com>
2019-09-03 13:30:13 +0200

    add the needed langugages to json file

--- extractFormat.py
+++ extractFormat.py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-import codecs
+# import codecs
+import json
+# import sys
+
 from iso639 import languages
 from pprint import pprint
 from googletrans import Translator
@@ -11,10 +14,15 @@ translator = Translator(service_urls=[
     ])
 
 # All the language codes that we will extract them
-lang_codes = [
-       'ja',
-       'es'
-]
+lang_codes =[]
+
+codes_files = open('languages_codes.txt' , "r")
+if codes_files.mode == 'r':
+  lines = codes_files.readlines()
+  for lang_code in lines:
+    lang_code = lang_code.strip()
+    if not lang_code: continue
+    lang_codes.append(lang_code)
 
 
 # used to create the list items
@@ -26,6 +34,14 @@ def getListItems(lang_code):
   """ returns the list of items that needed for dijunction/conjunction
   list formatter.
   """
+  try: 
+    translator.translate('English' , dest=lang_code).text
+    (languages.get(alpha2=lang_code)).name
+  except:
+
+    print('This languages is hard to find the list items for it: \"' + lang_code +'\"')
+    return None
+
   names_list = []
   languages_list = []
   for name in essential_names:
@@ -33,6 +49,7 @@ def getListItems(lang_code):
   
   for lang_name in essential_languages: 
     languages_list.append(translator.translate(lang_name , dest=lang_code).text)
+  
   # add the language name to the languages list
   lang_name_of_code = (languages.get(alpha2=lang_code)).name
   languages_list.append(translator.translate(lang_name_of_code , dest=lang_code).text)
@@ -40,14 +57,20 @@ def getListItems(lang_code):
   return {
     'lang_code': lang_code,
     'lang_name_of_code': lang_name_of_code,
+    'name_list_english' : essential_names,
     'names_list': names_list,
+    'languages_list_english' : essential_languages + [lang_name_of_code],
     'languages_list' : languages_list
   }
 
 
-
-  
+result = []
 
 for lang_code in lang_codes:
-	list_names = getListItems(lang_code)
-	print (list_names)
+  items_list = getListItems(lang_code)
+  if items_list:
+    result.append( getListItems(lang_code))
+	
+output = open('items_list_for_all_languages.json' , 'w+')
+output.write(json.dumps(result))
+output.close()
