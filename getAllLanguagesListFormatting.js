d2bb41806f0666952dbffcc8059179bd1356339c
Younies Mahmoud <younies@younies-imacpro.roam.corp.google.com>
2019-09-03 13:43:44 +0200

    clarify the input and the output of getAllLangugaes

--- getAllLanguagesListFormatting.js
+++ getAllLanguagesListFormatting.js
@@ -1,16 +1,21 @@
 /**
  * returns list format for all the list formats
  *   input: list of objects in form {
- *			langCode: string,
-   			langName: string,
-   			listOfItems: list of strings,
+   			'lang_code': string,
+		    'lang_name_of_code': string,
+		    'name_list_english' : list of strings,
+		    'names_list': list of strings,
+		    'languages_list_english' : list of strings,
+		    'languages_list' : list of strings
  *          }
 
      return: list of object in form {
 	    langCode: string,
     	langName : string,
-    	conjunction : string, // conjucate of the listOfItems
-    	disjunction: string   // disjuncate of the listOfItems
+    	conjunction_names : string, // conjucate of the listOfItems contains the names
+    	conjunction_languages : string, // conjucate of the listOfItems contains the langugaes
+    	disjunction_names: string   // disjuncate of the listOfItems contains the names
+    	disjunction_languages: string // disjuncate of the listOfItems contains the languages
      }  
  */
 getAllListsLanguages = (langList) => { 
